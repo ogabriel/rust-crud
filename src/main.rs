5097b3621474a3e632b2ef28affabfd828b981a5
ogabriel <gabrieloliver8991@gmail.com>
2025-10-05 12:07:30 -0300

    chore: add axum
    cargo add axum
    cargo add tokio --features full

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::delete, routing::get, routing::post, routing::put};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let app = Router::new()
+        .route("/pet", post(create_pet))
+        .route("/pet/:pet_id", get(get_pet))
+        .route("/pet", put(update_pet))
+        .route("/pet/:pet_id", delete(delete_pet));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn get_pet() {}
+async fn create_pet() {}
+async fn find_pet() {}
+async fn find_pet_by_status() {}
+async fn update_pet() {}
+async fn delete_pet() {}
