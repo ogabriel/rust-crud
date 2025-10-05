c925a66ea2876be73c1b4e81138d0615b3d2cf43
ogabriel <gabrieloliver8991@gmail.com>
2025-10-05 12:35:37 -0300

    feat: add base endpoint to API

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
+use axum::extract::Path;
 use axum::{Router, routing::delete, routing::get, routing::post, routing::put};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
     let app = Router::new()
-        .route("/pet", post(create_pet))
-        .route("/pet/:pet_id", get(get_pet))
-        .route("/pet", put(update_pet))
-        .route("/pet/:pet_id", delete(delete_pet));
+        .route("/pets", get(list_pets))
+        .route("/pets", post(create_pet))
+        .route("/pets/{pet_id}", get(find_pet))
+        .route("/pets/findByStatus", get(find_pet_by_status))
+        .route("/pets/{pet_id}", put(update_pet))
+        .route("/pets/{pet_id}", delete(delete_pet));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_pet() {}
+async fn list_pets() {}
 async fn create_pet() {}
-async fn find_pet() {}
+async fn find_pet(Path(pet_id): Path<String>) {}
 async fn find_pet_by_status() {}
-async fn update_pet() {}
-async fn delete_pet() {}
+async fn update_pet(Path(pet_id): Path<String>) {}
+async fn delete_pet(Path(pet_id): Path<String>) {}
